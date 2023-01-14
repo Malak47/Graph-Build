f583e39907584425797d2a411a591c53a986edd9
malak <malak.qeedan@hotmail.com>
2023-01-14 17:59:46 +0200

    adding javadoc

--- edges.h
+++ edges.h
@@ -14,16 +14,34 @@
 void insert_edge(pnode source, pnode dest, int weight);
 
 /**
- * Delete the edge between two nodes.
- * @param source: The tail of the edge.
- * @param dest: The head of the edge.
+ * Delete all the edges of node (inner and outer).
+ * @param head:  Pointer to the head of the linked list.
  */
-void delete_edge(pnode source, pnode dest);
+void delete_all_edges(pnode head);
 
 /**
- * Deletes all the edges of a given node.
- * @param node: The node to delete it's edges.
+ * Delete the outer edges of node.
+ * @param node: The node to delete from.
  */
-void delete_all_edges(pnode node);
+void delete_outer_edges(pnode node);
+
+/**
+ * Delete the inner edges of node.
+ * @param head: Pointer to the head of the linked list.
+ * @param node: The node to delete from.
+ */
+void delete_inner_edges(pnode head,pnode node);
+
+/**
+ * Prints out all the nodes with its edges and weights.
+ * @param head: Pointer to the head of the linked list.
+ */
+void print_all_edges(pnode head);
+
+/**
+ * Prints out node with its edges and weights.
+ * @param head: Pointer to the node to be printed.
+ */
+void print_edges(pnode node);
 
 #endif //S_S_A_4_EDGES_H
