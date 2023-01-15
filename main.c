39392d0c118fa0b03d175998fd6d7fd079f4c1ea
malak <malak.qeedan@hotmail.com>
2023-01-15 02:15:34 +0200

    creating main

--- main.c
+++ main.c
@@ -1,24 +1,50 @@
+
+#include "graph.h"
+#include "nodes.h"
 #include <stdio.h>
 
 int main() {
-    //Directed Graph.
-    //read from the user graph with unknown length.
-    //A -> stands for new graph, if older graph exists, remove it.
-    //number -> stands for a number of the nodes.
-    //after entering the number of the nodes, we got AT LEAST 3 inputs //
-        // [int,int,int] which is: the curr node, the dest node, the wight.
-    //n -> to switch between the nodes.
-
-    //B -> stands for a new node followed by [int] for it's number //
-        // and [int,int,...] for the dest nodes + it's wights.
-    // if the node is already exists, REMOVE all the directions that is OUT FROM IT.
-
-    //D -> deletes a node with its wights and directions.
-
-    //S [int,int] -> returns the wight between the given nodes (return -1 if not exists)
-
-    //P [int,int] -> returns the shortest path between the given nodes
-
-    //T [int_len, int, int, ... , int] -> input: T with length and length numbers.
+    pnode graph = NULL;
+    char input;
+    int stop = 1;
+    int ask = 0;
+    scanf(" %c", &input);
+    while (stop) {
+        if (ask == 1) {
+            scanf(" %c", &input);
+        }
+        switch (input) {
+            case 'A':
+                build_graph_cmd(&graph);
+                input = n_input(&graph);
+                ask = 0;
+                break;
+            case 'B':
+                insert_node_cmd(&graph);
+                ask = 1;
+                break;
+            case 'D':
+                delete_node_cmd(&graph);
+                ask = 1;
+                break;
+            case 'P':
+                printGraph_cmd(graph);
+                ask = 1;
+                break;
+            case 'S':
+                shortsPath_cmd(graph);
+                ask = 1;
+                break;
+            case 'T':
+                TSP_cmd(graph);
+                ask = 1;
+                break;
+            default:
+                stop = 0;
+                deleteGraph_cmd(&graph);
+                break;
+        }
+    }
+    deleteGraph_cmd(&graph);
     return 0;
 }
