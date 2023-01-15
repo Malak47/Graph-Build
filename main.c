cfce5e154cd861c867cec4012d689ef410e2677b
Malak Qeedan <75528759+Malak47@users.noreply.github.com>
2023-01-15 05:37:55 +0200

    Fixing main.

--- main.c
+++ main.c
@@ -1,44 +1,52 @@
+//
+// Created by malak on 1/10/2023.
+//
+#include "graph.h"
+#include "nodes.h"
+#include <stdio.h>
+
 int main() {
     pnode graph = NULL;
-
     char input;
+    int ask = 1;
     while (1) {
-        int input_result = scanf(" %c", &input);
-        if (input_result != 1) {
-            break;
+        if (ask) {
+            int input_result = scanf(" %c", &input);
+            if (input_result == -1 || input_result == 0) {
+                break;
+            }
         }
-
         switch (input) {
             case 'A':
-                input = build_graph_cmd(&graph);
+                build_graph_cmd(&graph);
+                input = n_input(&graph);
+                ask = 0;
                 break;
-
             case 'B':
                 insert_node_cmd(&graph);
+                ask = 1;
                 break;
-
             case 'D':
                 delete_node_cmd(&graph);
+                ask = 1;
                 break;
-
             case 'P':
                 printGraph_cmd(graph);
+                ask = 1;
                 break;
-
             case 'S':
                 shortsPath_cmd(graph);
+                ask = 1;
                 break;
-
             case 'T':
                 TSP_cmd(graph);
+                ask = 1;
                 break;
-
             default:
-                printf("Invalid input. Please try again.\n");
                 break;
         }
     }
     deleteGraph_cmd(&graph);
-
     return 0;
 }
+
